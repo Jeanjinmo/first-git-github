51afcbec7c80526a8c16dcc916bbce251e1377c0
jean jean <jeanel.karr@gmail.com>
2022-12-20 17:17:01 +0700

    first commit Tambah

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,3 +2,7 @@ fun main() {
     println("Kalkulator dan Git")
     println("Masukkan Angka 1 : " )
 }
+//method Tambah
+fun tambah(input_1: Double, input_2: Double): Double{
+    return input_1 + input_2
+}
