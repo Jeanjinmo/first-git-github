ea52fd18da64444f4420dad45379db8f97539802
jean jean <jeanel.karr@gmail.com>
2022-12-20 17:30:27 +0700

    first commit bagi

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 fun main() {
     println("Kalkulator dan Git")
-    println("Masukkan Angka 1 : " )
+    println("Masukkan Angka 1 : ")
+}
+
+//method bagi
+fun bagi(bil1: Double, bil2: Double): Double {
+    return bil1 / bil2
 }
